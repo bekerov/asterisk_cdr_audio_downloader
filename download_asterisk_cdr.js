13fa640afda254c8dbfc99b54b54656245e67e25
Bekerov Artur <bekerov.artur@yandex.ru>
2014-08-29 11:08:24 +0700

    Updated

--- download_asterisk_cdr.js
+++ download_asterisk_cdr.js
@@ -1,7 +1,7 @@
 /**
  * Created by Artur Bekerov on 29.08.14.
  */
-alert("Downloader loaded")
+alert("Downloader loaded");
 function cdr_play(a, b) {
     //cdr_play function redefined and then fetch data from iframe.
     // After fetched data pass to setAudio() function and create html element.
@@ -12,7 +12,7 @@ function cdr_play(a, b) {
 }
 function setAudio(data,a){
     src = window.location.protocol+'//'+window.location.host+'/admin/'+$($(data).find('embed')['prevObject'][7]).attr('src');
-    audio = '<audio class="audio_element" controls><source src="'+src+'" type="audio/wav">Упс. Твой браузер устарел.</audio>'
-    $('.audio_element').remove()
+    audio = '<audio class="audio_element" controls><source src="'+src+'" type="audio/wav">Oops. Browser is not support this feature.</audio>'
+    $('.audio_element').remove();
     $('#cdr_table tbody tr').eq(a-1).after(audio);
 }
